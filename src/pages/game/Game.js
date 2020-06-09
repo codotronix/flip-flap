607493bf36677b5ebc1aba46559c6944f0ae5e3c
Suman Barick <codotronix@gmail.com>
2020-06-10 03:43:28 +0530

    flipping implemented

--- src/pages/game/Game.js
+++ src/pages/game/Game.js
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
+import clsx from 'clsx'
 import styles from './style.module.scss'
 
 const Game = props => {
+    const [ flippedCards, setFlippedCards ] = useState({})
+
     const boxes = new Array(16).fill(9)
+
+    const flipCard = i => setFlippedCards({
+        ...flippedCards,
+        [i]: !flippedCards[i]
+    })
+
     return (
         <div>
 
@@ -10,7 +19,23 @@ const Game = props => {
             {
                 boxes.map( (b, i) => 
                     <div key={i} className={styles.box}>
-                        {i}
+                        <div 
+                            className={clsx(styles.boxInner, flippedCards[i] && styles.isFlipped)}
+                            onClick={() => flipCard(i)}
+                        >
+                            <div 
+                                className={clsx(styles.cardFace, styles.cardFaceFront)}
+                                onClick={() => flipCard(i)}
+                            >
+                                front
+                            </div>
+
+                            <div 
+                                className={clsx(styles.cardFace, styles.cardFaceBack)}
+                            >
+                                {i}
+                            </div>
+                        </div>
                     </div>
                 )
             }
