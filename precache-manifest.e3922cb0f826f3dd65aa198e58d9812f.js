5f7b813a0c080df8d7acbf17f4883a2416a8a154
Suman Barick <codotronix@gmail.com>
2020-06-11 02:47:22 +0530

    Updates

---
+++ precache-manifest.e3922cb0f826f3dd65aa198e58d9812f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a157753f28d2635c6c6110722827068a",
+    "url": "/flip-flap/index.html"
+  },
+  {
+    "revision": "a846d4c83c51df4ec25b",
+    "url": "/flip-flap/static/css/main.12a6261a.chunk.css"
+  },
+  {
+    "revision": "91772505a70342065e37",
+    "url": "/flip-flap/static/js/2.7cc07aa1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/flip-flap/static/js/2.7cc07aa1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a846d4c83c51df4ec25b",
+    "url": "/flip-flap/static/js/main.51be3dba.chunk.js"
+  },
+  {
+    "revision": "9feb9e3fac9d48509a50",
+    "url": "/flip-flap/static/js/runtime-main.58a57a92.js"
+  }
+]);
